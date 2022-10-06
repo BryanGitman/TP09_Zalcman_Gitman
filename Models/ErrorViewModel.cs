6e87b27016925305a6e0b9329f6db69a7d5fc21a
MATIAS LEONEL ZALCMAN <47028030@ort>
2022-10-06 12:12:53 -0300

    Cree el proyecto y agregue logo, fotos de perfil y bd

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP09_Zalcman_Gitman.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
